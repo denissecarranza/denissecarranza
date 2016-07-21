70663fc19dbc5693df1f7e00a00accb71775605f
denissecarranza <denissecarranza2011@gmail.com>
2016-07-21 07:01:35 -0700

    Add files via upload

---
+++ jsportfolio.js
@@ -0,0 +1,6 @@
+function changeBackground(){	
+	if (document.body.style.backgroundImage = "url('pa.jpg')"){
+		document.body.style.backgroundImage = "url('disney.jpg')";
+	}
+	else{
+document.body.style.backgroundImage = "url('pa.jpg')";}
